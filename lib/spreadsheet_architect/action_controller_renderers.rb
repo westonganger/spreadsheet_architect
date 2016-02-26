5dae07497b42f5a5ed2b2f44c574dce0bec4ec63
Weston Ganger <westonganger@gmail.com>
2016-02-26 10:37:17 -0800

    fix renderers

--- lib/spreadsheet_architect/action_controller_renderers.rb
+++ lib/spreadsheet_architect/action_controller_renderers.rb
@@ -1,12 +1,12 @@
 if defined? ActionController
   ActionController::Renderers.add :xlsx do |data, options|
-    send_data data, type: :xlsx, disposition: :attachment, filename: "#{options[:filename] ? options[:filename].sub(".xlsx",'') || 'data'}.xlsx"
+    send_data data, type: :xlsx, disposition: :attachment, filename: "#{options[:filename] ? options[:filename].sub('.xlsx','') : 'data'}.xlsx"
   end
   ActionController::Renderers.add :ods do |data, options|
-    send_data data, type: :ods, disposition: :attachment, filename: "#{options[:filename] ? options[:filename].sub(".ods",'') || 'data'}.ods"
+    send_data data, type: :ods, disposition: :attachment, filename: "#{options[:filename] ? options[:filename].sub('.ods','') : 'data'}.ods"
   end
   ActionController::Renderers.add :csv do |data, options|
-    send_data data, type: :csv, disposition: :attachment, filename: "#{options[:filename] ? options[:filename].sub(".csv",'') || 'data'}.csv"
+    send_data data, type: :csv, disposition: :attachment, filename: "#{options[:filename] ? options[:filename].sub('.csv','') : 'data'}.csv"
   end
 
 
