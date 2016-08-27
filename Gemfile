78fb4fdf703f861260a4400d17d9ce3ecfbeb95b
Weston Ganger <westonganger@gmail.com>
2016-08-26 20:58:30 -0700

    briefly setup testing

--- Gemfile
+++ Gemfile
@@ -2,9 +2,8 @@ source 'https://rubygems.org'
 gemspec
 
 gem 'axlsx'
-gem 'axlsx_styler', git: 'https://github.com/westonganger/axlsx_styler.git', branch: :merge_styles
-
-gem 'rodf', git: 'https://github.com/thiagoarrais/rodf.git', branch: :develop
+gem 'axlsx_styler', github: 'westonganger/axlsx_styler', branch: :merge_styles
+gem 'rodf', github: 'thiagoarrais/rodf', branch: :develop
 
 group :rails do
   gem 'rails', '>= 3.2.0'
--- test/tc_spreadsheet_architect.rb
+++ test/tc_spreadsheet_architect.rb
@@ -26,6 +26,13 @@ class TestSpreadsheetArchitect < MiniTest::Test
       range_styles: [],
       borders: []
     }
+
+    @headers = ['test1', 'test2','test3']
+    @data = [
+      ['row1'],
+      ['row2 c1', 'row2 c2'],
+      ['the','data']
+    ]
   end
 
   def test_utils_methods
@@ -47,31 +54,45 @@ class TestSpreadsheetArchitect < MiniTest::Test
 
     #SpreadsheetArchitect::Utils.get_options(options, SpreadsheetArchitect)
       
-    eql(SpreadsheetArchitect::Utils.convert_styles_to_axlsx({background_color: '333333', color: '000000', align: true, bold: true, font_size: 14, italic: true, underline: true, test: true}), {bg_color: '333333', fg_color: '000000', alignment: {horizontal: true}, b: true, sz: 14, i: true, u: true, test: true})
+    styles = SpreadsheetArchitect::Utils.convert_styles_to_axlsx({background_color: '333333', color: '000000', align: true, bold: true, font_size: 14, italic: true, underline: true, test: true})
+    eql(styles, {bg_color: '333333', fg_color: '000000', alignment: {horizontal: true}, b: true, sz: 14, i: true, u: true, test: true})
   end
 
-  def test_csv_methods
-    headers = ['test1', 'test2','test3']
-    data = [
-      ['row1'],
-      ['row2 c1', 'row2 c2'],
-      ['the','data']
-    ]
+  def test_xlsx
+    File.open('tmp/test.xlsx','w+') do |f|
+      f.write SpreadsheetArchitect.to_xlsx(headers: @headers, data: @data)
+    end
 
-    #Post.to_csv
-    SpreadsheetArchitect.to_csv(headers: headers, data: data)
-
-    #Post.to_ods
-    SpreadsheetArchitect.to_ods(headers: headers, data: data)
-
-    #Post.to_rodf_spreadsheet
-    SpreadsheetArchitect.to_rodf_spreadsheet(headers: headers, data: data)
+    File.open('tmp/multi-sheet.xlsx','w+') do |f|
+      package = SpreadsheetArchitect.to_axlsx_package({data: @data, headers: @headers})
+      package = SpreadsheetArchitect.to_axlsx_package({data: @data, headers: @headers, sheet_name: 'The Sheet Name'}, package)
+      f.write package.to_stream.read
+    end
 
     #Post.to_xlsx
-    SpreadsheetArchitect.to_xlsx(headers: headers, data: data)
+    #Post.to_axlsx_package({data: @data, headers: @headers})
+  end
 
-    #Post.to_axlsx_package({data: data, headers: headers})
-    package = SpreadsheetArchitect.to_axlsx_package({data: data, headers: headers})
-    SpreadsheetArchitect.to_axlsx_package({data: data, headers: headers, sheet_name: 'The Sheet Name'}, package)
+  def test_ods
+    File.open('tmp/test.ods','w+') do |f|
+      f.write SpreadsheetArchitect.to_ods(headers: @headers, data: @data)
+    end
+
+    File.open('tmp/multi-sheet.ods','w+') do |f|
+      spreadsheet = SpreadsheetArchitect.to_rodf_spreadsheet(headers: @headers, data: @data)
+      spreadsheet = SpreadsheetArchitect.to_rodf_spreadsheet({headers: @headers, data: @data}, spreadsheet)
+      f.write spreadsheet.bytes
+    end
+
+    #Post.to_ods
+    #Post.to_rodf_spreadsheet
+  end
+
+  def test_csv
+    File.open('tmp/test.csv','w+') do |f|
+      f.write SpreadsheetArchitect.to_csv(headers: @headers, data: @data)
+    end
+    
+    #Post.to_csv
   end
 end
