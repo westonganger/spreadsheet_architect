821fe94b4f16d512e022179607255310cb0666c9
Weston Ganger <weston@westonganger.com>
2019-11-27 09:26:07 -0800

    Fix test suite for Rails 6

--- test/dummy_app/config/application.rb
+++ test/dummy_app/config/application.rb
@@ -61,7 +61,7 @@ module Dummy
 
     if ActiveRecord.respond_to?(:gem_version)
       gem_version = ActiveRecord.gem_version
-      if gem_version >= Gem::Version.new("5.2")
+      if gem_version.to_s.starts_with?("5.2.")
         config.active_record.sqlite3.represent_boolean_as_integer = true
       end
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,9 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy_app/config/environment.rb",  __FILE__)
 
 migration_path = Rails.root.join('db/migrate')
-if ActiveRecord.gem_version >= ::Gem::Version.new("5.2.0")
+if ActiveRecord.gem_version >= ::Gem::Version.new("6.0.0")
+  ActiveRecord::MigrationContext.new(migration_path, ActiveRecord::SchemaMigration).migrate
+elsif ActiveRecord.gem_version >= ::Gem::Version.new("5.2.0")
   ActiveRecord::MigrationContext.new(migration_path).migrate
 else
   ActiveRecord::Migrator.migrate(migration_path)
