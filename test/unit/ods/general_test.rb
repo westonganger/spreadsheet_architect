a8c3b1eb0186dae441a7737ae70ff5445d467376
Weston Ganger <weston@westonganger.com>
2022-12-01 19:46:53 -0800

    Move kitchen sink examples to top of test file

--- test/unit/ods/general_test.rb
+++ test/unit/ods/general_test.rb
@@ -2,16 +2,6 @@ require "test_helper"
 
 class OdsGeneralTest < ActiveSupport::TestCase
 
-  test "multisheet" do
-    test_data = [[1,2,3], [4,5,6], [7,8,9]]
-
-    spreadsheet = Post.to_rodf_spreadsheet
-    spreadsheet = CustomPost.to_rodf_spreadsheet({sheet_name: 'Latest Projects'}, spreadsheet)
-    spreadsheet = SpreadsheetArchitect.to_rodf_spreadsheet({data: test_data, sheet_name: 'Another Sheet'}, spreadsheet)
-
-    save_file("ods/multi_sheet.ods", spreadsheet.bytes)
-  end
-
   test "kitchen sink" do
     options = {
       headers: [
@@ -38,6 +28,16 @@ class OdsGeneralTest < ActiveSupport::TestCase
     file_data = SpreadsheetArchitect.to_ods(options)
 
     save_file("ods/kitchen_sink.ods", file_data)
+  end
+
+  test "multisheet" do
+    test_data = [[1,2,3], [4,5,6], [7,8,9]]
+
+    spreadsheet = Post.to_rodf_spreadsheet
+    spreadsheet = CustomPost.to_rodf_spreadsheet({sheet_name: 'Latest Projects'}, spreadsheet)
+    spreadsheet = SpreadsheetArchitect.to_rodf_spreadsheet({data: test_data, sheet_name: 'Another Sheet'}, spreadsheet)
+
+    save_file("ods/multi_sheet.ods", spreadsheet.bytes)
   end
 
   describe "column_types" do
